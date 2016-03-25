37aa063cc9213993a325a66635c278768edae700
kanno-tatsunori <tatsunori.kanno@qd-ent.co.jp>
2016-03-25 11:19:06 +0900

    計算式の省略 画面サイズや原点を使用した計算を、変数に計算結果を入れることで省略 テスト画像やボタン画像も原点から計算するように変更

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -32,8 +32,18 @@ bool HelloWorld::init()
 		return false;
 	}
 
+	// 画面サイズ
 	Size visibleSize = Director::getInstance()->getVisibleSize();
-	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+	// 画面の半分のサイズ
+	Size visibleCenter = Size(visibleSize.width / 2, visibleSize.height / 2);
+
+	// 端末左下の位置
+	Vec2 origin = Director::getInstance()->getVisibleOrigin( );
+
+	// 端末ごとのサイズ差によるズレを考慮した画面サイズ
+	Vec2 visibleSizeOrigin = (Vec2)visibleSize + origin;
+	Vec2 visibleCenterOrigin = (Vec2)visibleCenter + origin;
 
 	/////////////////////////////
 	// 2. add a menu item with "X" image, which is clicked to quit the program
@@ -45,7 +55,7 @@ bool HelloWorld::init()
 		"CloseSelected.png",
 		CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
 
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width / 2,
+	closeItem->setPosition(Vec2(visibleSizeOrigin.x - closeItem->getContentSize().width / 2,
 		origin.y + closeItem->getContentSize().height / 2));
 
 	// create menu, it's an autorelease object
@@ -62,8 +72,8 @@ bool HelloWorld::init()
 	auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
 
 	// position the label on the center of the screen
-	label->setPosition(Vec2(origin.x + visibleSize.width / 2,
-		origin.y + visibleSize.height - label->getContentSize().height));
+	label->setPosition(Vec2(visibleCenterOrigin.x,
+		visibleSizeOrigin.y - label->getContentSize().height));
 
 	// add the label as a child to this layer
 	this->addChild(label, 1);
@@ -72,14 +82,14 @@ bool HelloWorld::init()
 	auto sprite = Sprite::create("HelloWorld.png");
 
 	// position the sprite on the center of the screen
-	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	sprite->setPosition(Vec2(visibleCenterOrigin.x, visibleCenterOrigin.y));
 
 	// add the sprite as a child to this layer
 	this->addChild(sprite, 0);
 
 	m_TestSprite = Sprite::create("Koala.jpg");
 
-	m_TestSprite->setPosition(Vec2(m_TestSprite->getContentSize().width / 2, visibleSize.height / 2 + origin.y));
+	m_TestSprite->setPosition(Vec2(m_TestSprite->getContentSize().width / 2 + origin.x, visibleCenterOrigin.y));
 
 	this->addChild(m_TestSprite, 0);
 
@@ -89,7 +99,7 @@ bool HelloWorld::init()
 		"ButtonStop.png",
 		CC_CALLBACK_1(HelloWorld::deleteTestSprite, this));
 
-	buttonItem->setPosition(Vec2(visibleSize.width - buttonItem->getContentSize().width / 2, visibleSize.height/2 + origin.y));
+	buttonItem->setPosition(Vec2(visibleSizeOrigin.x - buttonItem->getContentSize().width / 2, visibleCenterOrigin.y));
 	
 	//メニューを作成 自動解放オブジェクト
 	auto button_menu = Menu::create(buttonItem, NULL);
