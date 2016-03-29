6ba5d4e567e6c3277e9c416c6d942b218d46dde3
kanno-tatsunori <tatsunori.kanno@qd-ent.co.jp>
2016-03-29 11:08:32 +0900

    プレイヤーの移動処理の追加 左右キーでプレイヤーが左右に移動するように追加

--- Classes/CGameScene/CPlayer/CPlayer.cpp
+++ Classes/CGameScene/CPlayer/CPlayer.cpp
@@ -3,6 +3,8 @@
 USING_NS_CC;
 
 CPlayer::CPlayer()
+	: m_Speed(5.0f)
+	, m_MoveDirection(eMOVE_DIRECTION_NONE)
 {
 }
 
@@ -15,5 +17,72 @@ bool CPlayer::init()
 
 	this->initWithFile("Player.png");
 
+	control();
+
+	this->scheduleUpdate();
+
 	return true;
+}
+
+// 更新
+void CPlayer::update(float delta)
+{
+	// 移動
+	move();
+
+	// 移動制限
+	moveLimit();
+}
+
+// 操作
+void CPlayer::control()
+{
+	auto dispatcher = Director::getInstance()->getEventDispatcher();
+	auto listener = EventListenerKeyboard::create();
+
+	// 左右キーを押した時、移動を開始する
+	listener->onKeyPressed = [&](EventKeyboard::KeyCode keyCode, Event* event) {
+		if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_RIGHT_ARROW) {
+			m_MoveDirection = eMOVE_DIRECTION_RIGHT;
+		}
+		else if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_LEFT_ARROW) {
+			m_MoveDirection = eMOVE_DIRECTION_LEFT;
+		}
+	};
+
+	// 左右キーを離した時、移動をやめるようにする
+	listener->onKeyReleased = [&](EventKeyboard::KeyCode keyCode, Event* event) {
+		if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_RIGHT_ARROW && m_MoveDirection == eMOVE_DIRECTION_RIGHT) {
+			m_MoveDirection = eMOVE_DIRECTION_NONE;
+		}
+		else if (keyCode == cocos2d::EventKeyboard::KeyCode::KEY_LEFT_ARROW && m_MoveDirection == eMOVE_DIRECTION_LEFT) {
+			m_MoveDirection = eMOVE_DIRECTION_NONE;
+		}
+	};
+
+	dispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+	this->scheduleUpdate();
+}
+
+// 移動
+void CPlayer::move()
+{
+	switch (m_MoveDirection)
+	{
+	case eMOVE_DIRECTION_RIGHT:	this->runAction(MoveBy::create(0.0f, Vec2(m_Speed, 0.0f))); break;
+	case eMOVE_DIRECTION_LEFT:	this->runAction(MoveBy::create(0.0f, Vec2(-m_Speed, 0.0f))); break;
+	default:	break;
+	}
+}
+
+// 移動制限
+void CPlayer::moveLimit()
+{
+	Vec2 pos = this->getPosition();
+	Vec2 centerSize = this->getContentSize() / 2;
+
+	pos.x = std::min(pos.x, Director::getInstance()->getVisibleSize().width - centerSize.x);
+	pos.x = std::max(pos.x, 0.0f + centerSize.x);
+
+	this->setPosition(pos);
 }
--- Classes/CGameScene/CPlayer/CPlayer.h
+++ Classes/CGameScene/CPlayer/CPlayer.h
@@ -10,7 +10,25 @@ public:
 
 	virtual bool init() override;
 
+	virtual void update(float delta) override;
+
+	void control();
+
+	void move();
+
+	void moveLimit();
+
 	CREATE_FUNC(CPlayer);
+private:
+	// ˆÚ“®•ûŒü
+	enum eMOVE_DIRECTION{
+		eMOVE_DIRECTION_NONE
+		, eMOVE_DIRECTION_RIGHT
+		, eMOVE_DIRECTION_LEFT
+	};
+
+	eMOVE_DIRECTION	m_MoveDirection;	// ˆÚ“®•ûŒü
+	float			m_Speed;			// ˆÚ“®‘¬“x
 };
 
 #endif // __CPLAYER_H__
