ce6de21fe0c4b3a2fee11036a53003100a633b1d
kanno-tatsunori <tatsunori.kanno@qd-ent.co.jp>
2016-03-29 15:19:19 +0900

    弾の画像の追加 プレイヤーの弾の画像をZキーを押した時表示するよう追加

---
+++ Classes/CGameScene/CBullet/CBullet.cpp
@@ -0,0 +1,39 @@
+﻿#include "CBullet.h"
+
+USING_NS_CC;
+
+CBullet::CBullet()
+	: m_Speed(5.0f)
+{
+}
+
+// 初期化
+bool CBullet::init()
+{
+	if (!Sprite::init()) {
+		return false;
+	}
+
+	this->initWithFile("Bullet.png");
+
+	this->scheduleUpdate();
+
+	return true;
+}
+
+// 更新
+void CBullet::update(float delta)
+{
+	// 移動
+	move();
+}
+
+// 移動
+void CBullet::move()
+{
+	Vec2 pos = this->getPosition();
+
+	pos.y += m_Speed;
+
+	this->setPosition(pos);
+}
